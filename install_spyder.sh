07ef2966d3b24c9a39b57a62b17ed669e420879c
tomaskg <112718771+tomaskg@users.noreply.github.com>
2023-06-03 14:28:08 -0400

    Add user input functionality to allow customization of the environment name and alias.

--- install_spyder.sh
+++ install_spyder.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Ask for the name of the environment or use default value
+read -p "Enter a name for the environment (default: spyder-env): " env_name
+env_name=${env_name:-spyder-env}
+
+# Ask for the name of the shortcut or use default value
+read -p "Enter a name for the shortcut to run Spyder (default: spy): " alias_name
+alias_name=${alias_name:-spy}
+
 # Download and install Miniconda
 echo "Downloading and installing Miniconda..."
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
@@ -15,11 +23,11 @@ conda update -y -n base conda
 
 # Create a new environment and install Spyder
 echo "Creating a new environment with Spyder..."
-conda create -y -n spyder-env python=3.8 spyder
+conda create -y -n "$env_name" python=3.8 spyder
 
-# Create a "spy" alias to run Spyder
-echo "Creating a 'spy' shortcut to run Spyder..."
-echo 'alias spy="$HOME/miniconda/envs/spyder-env/bin/spyder"' >> $HOME/.bashrc
+# Create an alias to run Spyder
+echo "Creating a '$alias_name' shortcut to run Spyder..."
+echo "alias $alias_name=\"$HOME/miniconda/envs/$env_name/bin/spyder\"" >> $HOME/.bashrc
 source $HOME/.bashrc
 
-echo "Installation complete! Run 'spy' to start Spyder."
+echo "Installation complete! Run '$alias_name' to start Spyder."
